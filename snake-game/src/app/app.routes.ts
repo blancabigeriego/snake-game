2ab5a59d61391c405e01f36b68a2919f017ec628
unknown <blancabcbt@gmail.com>
2024-11-27 20:54:49 +0100

    Feat: add home component and routing

--- snake-game/src/app/app.routes.ts
+++ snake-game/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { HomeComponent } from './home/home.component';
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
 
-export const routes: Routes = [];
+    {
+        path: 'game',
+        component: GameComponent
+    }
+];
