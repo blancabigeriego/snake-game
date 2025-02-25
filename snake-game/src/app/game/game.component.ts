dae70dc395172c6444b78d9d8117c8006ad5c951
unknown <blancabcbt@gmail.com>
2025-02-25 21:11:27 +0100

    Add countdown

--- snake-game/src/app/game/game.component.ts
+++ snake-game/src/app/game/game.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink, NgIf],
   templateUrl: './game.component.html',
-  styleUrl: './game.component.css'
+  styleUrls: ['./game.component.css']
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
+
+  public isCountDown: boolean = false;
+  public countDownNumber: number = 3;
+
+  ngOnInit(): void {
+    this.startCountDown();
+  };
+
+  private startCountDown(): void {
+    if (typeof window !== 'undefined') {
+      // Esto asegura que solo se ejecute en el navegador
+      const beepSound = new Audio('assets/sounds/beep.wav');
+      this.isCountDown = true;
+
+      const interval = setInterval(() => {
+        if (this.countDownNumber > 0) {
+          beepSound.play(); // Reproduce el pitido solo si el número es mayor que 0
+          this.countDownNumber--;
+        } else if (this.countDownNumber === 0) {
+          beepSound.play(); // Si el número llega a 0, reproduce el pitido una última vez
+          clearInterval(interval); // Detenemos el intervalo
+          this.isCountDown = false;
+        }
+      }, 1000);
+    }
+  }
+
+
+
 
 }
